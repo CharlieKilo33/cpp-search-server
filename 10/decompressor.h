388dc58bcf636f0e9bd5837020977e872d9dfa32
mishak04@yandex.ru <Mike>
2024-01-21 04:05:06 +0300

    binaryfiles new

--- 10/decompressor.h
+++ 10/decompressor.h
@@ -3,35 +3,41 @@
 #include <string>
 #include <fstream>
 
-
-inline bool DecodeRLE(const std::string& src_name, const std::string& dst_name){
-  std::ifstream in_file(src_name, std::ios::binary);
-  if(!in_file.is_open()){
-    return false;
-  }
-
-  std::ofstream out_file(dst_name, std::ios::binary);
-  if(!out_file.is_open()){
-    return false;
-  }
-
-  unsigned char header = in_file.get();
-
-  while(in_file){
-    int block_type = (header & 1);
-    int data_size = (header >> 1) + 1;
-
-    if(block_type){
-      out_file << in_file.get();
-    } else {
-      std::string text(static_cast<char>(in_file.get()), data_size);
-      out_file << text;
-
-//      for(int i = 0; i < data_size; ++i){
-//        out_file << static_cast<char>(in_file.get());
-//      }
+inline bool DecodeRLE(const std::string& src_name, const std::string& dst_name) {
+    std::ifstream in(src_name, std::ios::binary);
+    if (!in) {
+        return false;
     }
-    header = in_file.get();
-  }
-  return true;
+
+    std::ofstream out(dst_name, std::ios::binary);
+    if (!out) {
+        return false;
+    }
+
+    unsigned char header;
+    while (in.get(reinterpret_cast<char&>(header))) {
+        int block_type = (header & 1);
+        int data_size = (header >> 1) + 1;
+
+        if (block_type) {
+            unsigned char data;
+            if (!in.get(reinterpret_cast<char&>(data))) {
+                return false;
+            }
+
+            std::string repeated_data(data_size, static_cast<char>(data));
+            out.write(repeated_data.c_str(), repeated_data.size());
+        } else {
+            char* buffer = new char[data_size];
+            if (!in.read(buffer, data_size)) {
+                delete[] buffer;
+                return false;
+            }
+
+            out.write(buffer, data_size);
+            delete[] buffer;
+        }
+    }
+
+    return true;
 }
